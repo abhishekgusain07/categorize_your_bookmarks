5d75b497869109096decc2a2c07090048b30e323
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-02-26 12:05:57 +0530

    chore

---
+++ src/db/schema.ts
@@ -0,0 +1,55 @@
+import { pgEnum, pgTable, serial, text, timestamp, uniqueIndex, boolean, integer } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+export const users = pgTable("users", {
+  id: text("id").primaryKey(),
+  createdTime: timestamp("created_time").defaultNow(),
+  email: text("email").unique(),
+  firstName: text("first_name"),
+  lastName: text("last_name"),
+  gender: text("gender"),
+  profileImageUrl: text("profile_image_url"),
+  userId: text("user_id").unique(),
+  subscription: text("subscription"),
+  credits: text("credits"),
+});
+
+
+export const subscriptions = pgTable("subscriptions", {
+    id: serial("id").primaryKey(),
+    createdTime: timestamp("created_time").defaultNow(),
+    subscriptionId: text("subscription_id"),
+    stripeUserId: text("stripe_user_id"),
+    status: text("status"),
+    startDate: text("start_date"),
+    endDate: text("end_date"),
+    planId: text("plan_id"),
+    defaultPaymentMethodId: text("default_payment_method_id"),
+    email: text("email"),
+    userId: text("user_id"),
+  });
+  
+export const subscriptionPlans = pgTable("subscriptions_plans", {
+    id: serial("id").primaryKey(),
+    createdTime: timestamp("created_time").defaultNow(),
+    planId: text("plan_id"),
+    name: text("name"),
+    description: text("description"),
+    amount: text("amount"),
+    currency: text("currency"),
+    interval: text("interval"),
+});
+
+export const invoices = pgTable("invoices", {
+    id: serial("id").primaryKey(),
+    createdTime: timestamp("created_time").defaultNow(),
+    invoiceId: text("invoice_id"),
+    subscriptionId: text("subscription_id"),
+    amountPaid: text("amount_paid"),
+    amountDue: text("amount_due"),
+    currency: text("currency"),
+    status: text("status"),
+    email: text("email"),
+    userId: text("user_id"),
+});
+  
